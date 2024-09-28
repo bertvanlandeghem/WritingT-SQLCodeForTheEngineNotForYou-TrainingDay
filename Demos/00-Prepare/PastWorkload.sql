20cf7bf8d50c1aaf89f292f591dbee120af768e0
ClaudioESSilva <claudiosil100@gmail.com>
2024-09-28 15:37:35 +0100

    Past workload

---
+++ Demos/00-Prepare/PastWorkload.sql
@@ -0,0 +1,27 @@
+/*
+    This should run on a past period (a couple of hours should be enough) so we can 
+    have some data to work and compare with.
+*/
+
+Use [StackOverflow]
+GO
+
+DECLARE @param1 INT = 1
+	  , @param2 INT = 2
+
+SELECT Id
+  FROM dbo.Users AS U
+ WHERE (
+              EXISTS (SELECT 1
+						FROM dbo.Posts AS P
+					   WHERE P.OwnerUserId = U.Id
+					     AND P.Score = @param1
+						 AND P.ViewCount = 10
+					 )
+           OR EXISTS (SELECT 1
+						FROM dbo.Posts AS P2
+					   WHERE P2.OwnerUserId = U.Id
+					     AND P2.Score = @param2
+						 AND P2.ViewCount = 10
+					 )
+        )
