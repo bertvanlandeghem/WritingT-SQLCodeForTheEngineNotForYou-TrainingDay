ebae9ca6ddc61a6384bb3fc6276cbb65dbdbb921
ClaudioESSilva <claudiosil100@gmail.com>
2024-09-28 16:06:21 +0100

    More examples

--- Demos/00-Prepare/Helpers/Monitoring.sql
+++ Demos/00-Prepare/Helpers/Monitoring.sql
@@ -1,3 +1,13 @@
+/*
+	Get the execution plan
+*/
 EXEC sp_WhoIsActive
     @get_plans = 1
+GO
+
+/*
+	The outer command that was called and have the current query running
+*/
+EXEC sp_WhoIsActive
+    @get_outer_command = 1
 GO
